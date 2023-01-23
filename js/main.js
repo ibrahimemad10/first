03dfa1466ccfa96e6666e13f732205ad29fac21c
ibrahimemad10 <ibrahimemad1516@gmail.com>
2023-01-24 01:22:07 +0200

    first app

---
+++ js/main.js
@@ -0,0 +1,22 @@
+// const acc_btns = document.querySelectorAll('.accordian-header');
+// const acc_cotents = document.querySelectorAll('.accordian-body');
+
+
+
+// acc_btns.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+
+//     acc_cotents.forEach((acc) => {
+//       if (
+//         e.target.nextElementSibling !== acc &&
+//         acc.classList.contains("active")
+//       ) {
+//         acc.classList.remove("active");
+//         acc_btns.forEach((btn) => btn.classList.remove("active"))
+//       }
+//     })
+//     const panel = btn.nextElementSibling;
+//     panel.classList.toggle("active");
+//     btn.classList.toggle("active");
+//   });
+// });
